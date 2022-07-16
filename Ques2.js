2bac430340141ed23f7e665f64ee9e537b3567bd
Abdul Mannan Khan <99967792+Abdulmannankhann@users.noreply.github.com>
2022-07-16 13:07:15 +0530

    Update Ques2.js

--- Ques2.js
+++ Ques2.js
@@ -5,7 +5,6 @@
 //example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7,
 //your program should return [[11,  -4], [2, 5]] because
 // 11 + -4 = 7 and 2 + 5 = 7.
-
 function sums(arr, sum) {
   let pairs = [];
   let numList = [];
